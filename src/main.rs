38c93b2f6b9a144227e682ae6909d570405e863e
nextor <nextor@gmail.com>
2022-04-29 23:15:08 -0700

    Starting svg

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,59 @@
 //extern crate warc;
-//use std::process;
+use std::process;
+use std::env;
 use warc::WarcReader;
 use lazy_static::lazy_static;
 use regex::Regex;
 //use html_parser::Dom;
 
 fn main(){
-    //let file = WarcReader::from_path("warc_example.warc")?;
-    let file = WarcReader::from_path_gzip("/home/nextor/Projects/CommonCrawl/file1.warc.gz").unwrap();
-    lazy_static! {
-        //let email_regex = Regex::new(r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})").unwrap();
-        static ref RE: Regex = Regex::new(r"(?x)
-            (?P<login>[^@\s\#/\{\}<>,\u0022!:;\[\]'\(\)]+@
-            #([[:word:]]+\.)+ Este es un comentario
-            ([^@\s\#/\{\}<>,\u0022!:;\[\]'\(\)%]+\.)+
-            [[:word:]]{2,28})
-            ").unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 
+    {
+        println!("Usage warc_parser warc_file.warc.gz");
+        process::exit(1);
     }
 
-    let mut counter:i64 = 0;
+    let filepath = args.get(1).expect("Usage warc_parser warc_file.warc.gz");
+
+
+    //let file = WarcReader::from_path("warc_example.warc")?;
+    let file = WarcReader::from_path_gzip( filepath ).expect("Not a warc valid format");
+    lazy_static! {
+        //let email_regex = Regex::new(r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})").unwrap();
+        //static ref RE: Regex = Regex::new(r"(?x)
+        //    (?P<login>[^@\s\#/\{\}<>,\u0022!:;\[\]'\(\)]+@
+        //    #([[:word:]]+\.)+ Este es un comentario
+        //    ([^@\s\#/\{\}<>,\u0022!:;\[\]'\(\)%]+\.)+
+        //    [[:word:]]{2,28})
+        //    ").unwrap();
+        //
+        //
+        static ref RE: Regex = Regex::new(r"(?xm)
+            (?P<svg><svg>.+</svg>)
+            ").unwrap();
+
+    }
+
 
     for record in file.iter_records() {
         match record {
             Err(_err) =>{},
             Ok(record) => {
-
-                counter += 1;
-                
-
                 let warkid = match record.header(warc::WarcHeader::TargetURI).map(|s| s.to_string()) {
                     Some(v)=>v,
                     None=> "".to_string()
                 };
 
-                //println!("Length from contentLength {}",record.content_length());
-
-
-                //mut iter = body.split_inclusive(|num| num % length );
-                //
-
                 let s:String = get_string(record.body());
 
                 for caps in RE.captures_iter(&s.to_string()){
-                    println!("{}\t{}",&caps["login"],warkid);
+
+                    println!("{}\t{}",&caps["svg"],warkid);
                 }
-
-                //if counter > 71862 
-                //{
-                //    println!("{} ",warkid);
-                //    println!("the shits ends {} ", s);
-                //    process::exit(1);
-                //}
-
            }
         }
-
-        //counter += 1;
-
-        //if counter == 3 
-        //{
-        //    process::exit(1);
-        //}
     }
 }
 
