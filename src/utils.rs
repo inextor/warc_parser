9910bd181e071ced796c27ae1c6b8fa10b89e5d8
nextor <nextor@gmail.com>
2022-05-12 22:41:13 -0700

    First version of utils

---
+++ src/utils.rs
@@ -0,0 +1,61 @@
+use libflate::gzip::MultiDecoder as GzipReader;
+use std::io::BufReader;
+use reqwest::blocking::Response;
+
+fn parse_url(url_string:String)->String
+{
+    let parts = url_string.split(')');
+    let vec:Vec<&str> = parts.collect();
+    let firsts = match vec.first() 
+    {
+        Some(some_str)=>some_str,
+        None=> ""
+    };
+
+    let parts = firsts.split(',');
+    let mut vec:Vec<&str> = parts.collect();
+    vec.reverse();
+    vec.join(".")
+}
+
+pub fn download_gzip_file(url:&str)->Result<GzipReader<BufReader<Response>>,&str>
+{
+    match reqwest::blocking::get(url)
+    {
+        Ok(response)=>
+        {
+            match GzipReader::new(BufReader::with_capacity(4*1_048_576, response ))
+            {
+                Ok(gzip_stream)=>{
+                    Ok(gzip_stream)
+                }
+                ,Err(gzip_err)=>
+                {
+                    eprintln!("Error {}",gzip_err);
+                    Err("")
+                }
+            }
+        },
+        Err(net_err)=>
+        { 
+            eprintln!("Error {}",net_err);
+            Err("")
+        }
+    }
+}
+
+pub fn u8_tostring(body:&[u8])->String
+{
+    let s = match String::from_utf8(body.to_vec())
+    {
+        Ok(ss)=>ss,
+        Err(_)=> String::from("")
+    };
+
+    if !s.is_empty()
+    {
+        return s;
+    };
+
+    return String::from_utf8_lossy( body ).to_string();
+}
